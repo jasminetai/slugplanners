4189995cf418769ce78146083fee1f863ae74ab3
Jasmine Tai <jasminecktai@gmail.com>
2022-01-14 22:50:45 -0800

    add initial js, web scraper js for catalog, json data for classes

---
+++ scripts/slugsurvival.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const axios = require('axios');
+
+axios
+  .get('https://andromeda.miragespace.net/slugsurvival/data/fetch/terms/2220.json')
+  .then(response => {
+    fs.writeFile('slugshort.json', JSON.stringify(response.data), function (e) {
+      if (e) {
+        console.log(e);
+      }
+    });
+  })
+  .catch(e => console.log(e));
+
+axios
+  .get('https://andromeda.miragespace.net/slugsurvival/data/fetch/courses/2220.json')
+  .then(response => {
+    fs.writeFile('sluglong.json', JSON.stringify(response.data), function (e) {
+      if (e) {
+        console.log(e);
+      }
+    });
+  })
+  .catch(e => console.log(e));
